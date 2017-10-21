df36a8d6c3dff6b72414259d165b597042ed02c4
Danielle Tomlinson <dan@tomlinson.io>
2017-10-21 20:38:46 +0100

    Refactor websocket handling

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from string import Template
 from auth import AzureAuthClient
 from xml.etree import ElementTree
 import nexmo
-
 from config import NEXMO_APPLICATION_ID, NEXMO_PRIVATE_KEY, WATSON_USERNAME, WATSON_PASSWORD, WATSON_URL, MICROSOFT_TRANSLATION_CLIENT_SECRET, HOSTNAME
+
+client = nexmo.Client(application_id=NEXMO_APPLICATION_ID, private_key=NEXMO_PRIVATE_KEY)
 
 language_model = 'en-UK_NarrowbandModel' # Specify the Narrowband model for your language
 
@@ -29,14 +30,12 @@ def gettoken():
 
     return token
 
-
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.content_type = 'text/plain'
-        self.write("Watson STT Example")
+        self.write("Hiii")
         self.finish()
-
 
 class CallHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
@@ -51,11 +50,10 @@ class CallHandler(tornado.web.RequestHandler):
         self.set_header("Content-Type", 'application/json; charset="utf-8"')
         self.finish()
 
-
 class EventHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def post(self):
-        print self.request.body
+        body = self.request.body
         self.content_type = 'text/plain'
         self.write('ok')
         self.finish()
@@ -75,39 +73,57 @@ class MessageHandler():
                         print('To be translated: ' + text)
                         translate(text, 'de')
 
+class WatsonHandler():
+    def __init__(self, token, model, callback):
+        uri = 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize?watson-token={}&model={}'.format(token, model)
+        self.watson_future = tornado.websocket.websocket_connect(uri, on_message_callback=self.on_message)
+        print("Got Future:", self.watson_future)
+        self.callback = callback
+        print("Got Callback:", self.callback)
 
-class WSHandler(tornado.websocket.WebSocketHandler):
-    watson_future = None
-    def open(self):
-        print("Websocket Call Connected")
-        uri = 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize?watson-token={}&model={}'.format(gettoken(), language_model)
-        self.watson_future = tornado.websocket.websocket_connect(uri, on_message_callback=self.on_watson_message)
+    def close(self):
+        ws = yield self.watson_future
+        data = {'action' : 'stop'}
+        ws.write_message(json.dumps(data), binary=False)
+        ws.close()
 
-    @gen.coroutine
-    def on_message(self, message):
-        watson = yield self.watson_future
+    def write_message(self, message):
+        print "watson.write_message"
+        ws = yield self.watson_future
         if type(message) == str:
-            watson.write_message(message, binary=True)
+            ws.write_message(message, binary=True)
         else:
             data = json.loads(message)
             data['action'] = "start"
             data['continuous'] = True
             data['interim_results'] = True
             print json.dumps(data)
-            watson.write_message(json.dumps(data), binary=False)
+            ws.write_message(json.dumps(data), binary=False)
+
+    def on_message(self, message):
+        print "watson.on_message"
+        self.callback(message)
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("Websocket Call Connected")
+        self.watson = WatsonHandler(gettoken(), language_model, self.tts_completed)
+        print("Hi???")
+
+    def tts_completed(self, new_message):
+        print("TTS COMPLETED")
+        h = MessageHandler(new_message)
+        result = h.handle()
+
+    @gen.coroutine
+    def on_message(self, message):
+        print("Received Message", self.watson, self.watson.write_message)
+        self.watson.write_message(message)
 
     @gen.coroutine
     def on_close(self):
         print("Websocket Call Disconnected")
-        watson = yield self.watson_future
-        data = {'action' : 'stop'}
-        watson.write_message(json.dumps(data), binary=False)
-        watson.close()
-
-    def on_watson_message(self, message):
-        handler = MessageHandler(message)
-        handler.handle()
-
+        self.watson.close()
 
 def translate(text, language):
     headers = {"Authorization ": microsoft_translator_bearer_token}
@@ -118,10 +134,8 @@ def translate(text, language):
     print("Translated: " + translation.text)
     return translation.text
 
-
 def speak(uuid, text, voice_name):
     response = client.send_speech(uuid, text=text, voice_name='Marlene')
-
 
 def main():
     static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
@@ -134,7 +148,6 @@ def main():
     http_server = tornado.httpserver.HTTPServer(application)
     port = int(os.environ.get("PORT", 8000))
     http_server.listen(port)
-    print gettoken()
     print "Running on port: " + str(port)
     tornado.ioloop.IOLoop.instance().start()
 
