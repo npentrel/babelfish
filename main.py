656797a8d73e0d170818e0985358262c3f333fc9
Danielle Tomlinson <dan@tomlinson.io>
2017-10-21 23:02:25 +0100

    Echo back from ms api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tornado.httpserver
+import tornado.httpclient
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -6,17 +7,16 @@ from tornado import gen
 import json
 import requests
 import os
+import struct
 from string import Template
 from auth import AzureAuthClient
 from xml.etree import ElementTree
 import nexmo
-from config import NEXMO_APPLICATION_ID, NEXMO_PRIVATE_KEY, MICROSOFT_TRANSLATION_CLIENT_SECRET, HOSTNAME
+import StringIO
+from config import NEXMO_APPLICATION_ID, NEXMO_PRIVATE_KEY, MICROSOFT_TRANSLATION_SPEECH_CLIENT_SECRET, HOSTNAME
 
 client = nexmo.Client(application_id=NEXMO_APPLICATION_ID, private_key=NEXMO_PRIVATE_KEY)
-
-language_model = 'en-UK_NarrowbandModel' # Specify the Narrowband model for your language
-
-auth_client = AzureAuthClient(MICROSOFT_TRANSLATION_CLIENT_SECRET)
+auth_client = AzureAuthClient(MICROSOFT_TRANSLATION_SPEECH_CLIENT_SECRET)
 microsoft_translator_bearer_token = 'Bearer ' + auth_client.get_access_token()
 
 class MainHandler(tornado.web.RequestHandler):
@@ -62,18 +62,69 @@ class MessageHandler():
                         print('To be translated: ' + text)
                         translate(text, 'en', 'de')
 
+def make_wave_header(frame_rate):
+    """
+    Generate WAV header that precedes actual audio data sent to the speech translation service.
+    :param frame_rate: Sampling frequency (8000 for 8kHz or 16000 for 16kHz).
+    :return: binary string
+    """
+
+    if frame_rate not in [8000, 16000]:
+        raise ValueError("Sampling frequency, frame_rate, should be 8000 or 16000.")
+
+    nchannels = 1
+    bytes_per_sample = 2
+
+    output = StringIO.StringIO()
+    output.write('RIFF')
+    output.write(struct.pack('<L', 0))
+    output.write('WAVE')
+    output.write('fmt ')
+    output.write(struct.pack('<L', 18))
+    output.write(struct.pack('<H', 0x0001))
+    output.write(struct.pack('<H', nchannels))
+    output.write(struct.pack('<L', frame_rate))
+    output.write(struct.pack('<L', frame_rate * nchannels * bytes_per_sample))
+    output.write(struct.pack('<H', nchannels * bytes_per_sample))
+    output.write(struct.pack('<H', bytes_per_sample * 8))
+    output.write(struct.pack('<H', 0))
+    output.write('data')
+    output.write(struct.pack('<L', 0))
+
+    data = output.getvalue()
+
+    output.close()
+
+    return data
+
 class WSHandler(tornado.websocket.WebSocketHandler):
+    SentHeader = False
     def open(self):
         print("Websocket Call Connected")
+        translate_from = 'de'
+        translate_to = 'en-US'
+        features = "Partial,TextToSpeech"
+        uri = "wss://dev.microsofttranslator.com/speech/translate?from={0}&to={1}&features={2}&api-version=1.0".format(translate_from, translate_to, features)
+        request = tornado.httpclient.HTTPRequest(uri, headers={
+            'Authorization': 'Bearer ' + auth_client.get_access_token(),
+        })
+        self.ws_future = tornado.websocket.websocket_connect(request, on_message_callback=self.tts_completed)
 
     def tts_completed(self, new_message):
-        print "TTS COMPLETED"
-        h = MessageHandler(new_message)
-        result = h.handle()
+        self.write_message(new_message)
 
     @gen.coroutine
     def on_message(self, message):
-        print "Received Message"
+        if self.SentHeader == False:
+            print "Sending wav header"
+            ws = yield self.ws_future
+            header = make_wave_header(16000)
+            ws.write_message(header, binary=True)
+            self.SentHeader = True
+
+        print "Sending sound to microsoft"
+        ws = yield self.ws_future
+        ws.write_message(message, binary=True)
 
     @gen.coroutine
     def on_close(self):
