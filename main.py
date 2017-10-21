82a5029a1ce526dad3e61032204025eac4973ded
npentrel <npentrel@gmail.com>
2017-10-21 15:41:26 +0100

    Basic flask endpoint

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+from flask import Flask, request, make_response
 import nexmo
+
 from config import NEXMO_APPLICATION_ID, NEXMO_PRIVATE_KEY
 
-
-def main():
-    client = setup_nexmo()
-
-    call_received = False
-    while not call_received:
-        response = client.get_calls()
-        print(response)
-        # response = client.get_call(uuid)
+client = nexmo.Client(application_id=NEXMO_APPLICATION_ID, private_key=NEXMO_PRIVATE_KEY)
+app = Flask(__name__)
 
 
-def setup_nexmo():
-    return nexmo.Client(application_id=NEXMO_APPLICATION_ID, private_key=NEXMO_PRIVATE_KEY)
+@app.route('/')
+def index():
+    return "Hello World!"
 
 
-main()
+@app.route('/call_received', methods=['GET', 'POST'])
+def call_received():
+    print("call_received")
+    if request.method == 'POST':
+        print(request)
+        return make_response("", 200)
+    else:
+        return make_response("Not a post request", 200)
+
+
+if __name__ == '__main__':
+    app.run()
